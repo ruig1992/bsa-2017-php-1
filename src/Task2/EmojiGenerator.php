ef1d2ab931fa0790c22727da090d8a9377a7cacb
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-05-23 10:44:57 +0000

    Finished Task 2

--- src/Task2/EmojiGenerator.php
+++ src/Task2/EmojiGenerator.php
@@ -4,8 +4,20 @@ namespace BinaryStudioAcademy\Task2;
 
 class EmojiGenerator
 {
+    private $emojis = [
+        "\u{1F600}",
+        "\u{1F603}",
+        "\u{1F604}",
+        "\u{1F601}",
+        "\u{1F606}",
+        "\u{1F605}",
+        "\u{1F602}"
+    ];
+
     public function generate()
     {
-        //TODO: Implement emoji generator using yield
+        for ($i = 0, $n = count($this->emojis); $i < $n; $i++) {
+            yield $this->emojis[$i];
+        }
     }
 }
